292ad3a94c088870f543bea0a6da32c2f1cc531c
emi <3godlewska@gmail.com>
2017-03-07 22:09:17 +0100

    dodalam wstepny model Usera

--- czyrek/czyrek/manager/models.py
+++ czyrek/czyrek/manager/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    full_name = models.CharField(max_length=30, blank=False)
+    lastlog = models.DateTimeField(auto_now=True)
+
