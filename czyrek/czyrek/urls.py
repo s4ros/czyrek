a4229d33ec22ac91f6c8194bf48f76780134f5ef
Lukasz Wojno <l.wojno@imagho.pl>
2017-03-07 20:39:57 +0100

    Fixed czyrek urls.py. /manager application works now.

--- czyrek/czyrek/urls.py
+++ czyrek/czyrek/urls.py
@@ -12,9 +12,9 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    from django.conf.urls import include, url
 """
-from django.conf.urls import include, url
-from czyrek.manager import urls
+from django.conf.urls import url, include
 from django.contrib import admin
 
 
