258378208be94d411a88eacc31fb1a74cc956479
Lukasz Wojno <l.wojno@imagho.pl>
2017-03-09 23:10:33 +0100

    Fixed broken Candidate Model fields

--- czyrek/czyrek/manager/models.py
+++ czyrek/czyrek/manager/models.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
-
+####################
+## Django imports
 from django.db import models
 
+####################
+## User Model
 class User(models.Model):
     login = models.CharField(max_length=50)
     passwd = models.CharField(max_length=100)
@@ -13,6 +16,28 @@ class User(models.Model):
     def __str__(self):
         return self.login + " (" + self.email + ")" + " - " + self.full_name
 
+####################
+## Schools Model
+class Schools(models.Model):
+    name = models.CharField(max_length=100)
+    is_available = models.BooleanField()
+
+####################
+## Languages Model
+class Languages(models.Model):
+    #nie wiem czy po tym bedzie schools?
+    school_id = models.ForeignKey(Schools)
+    name = models.CharField(max_length=30)
+    is_available = models.BooleanField()
+
+####################
+## Subjects Model
+class Subjects(models.Model):
+    is_available = models.BooleanField()
+    wage = models.IntegerField()
+
+####################
+## Candidate Model
 class Candidate(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
@@ -26,27 +51,16 @@ class Candidate(models.Model):
     birthdate = models.DateField()
     last_school = models.CharField(max_length=100)
     primary_language = models.ForeignKey(Languages)
-    secondary_language = models.ForeignKey(Languages)
+    secondary_language = models.ForeignKey(Languages,related_name="l2")
     subject_one = models.ForeignKey(Subjects)
-    subject_two = models.ForeignKey(Subjects)
-    subject_three = = models.ForeignKey(Subjects)
-    photo = models.CharField()
-
-class Schools(models.Model):
-    name = models.CharField(max_length=100)
-    is_available = models.BooleanField()
-
+    subject_two = models.ForeignKey(Subjects,related_name="s2")
+    subject_three = models.ForeignKey(Subjects,related_name="s3")
+    photo = models.CharField(max_length=100)
+    class Meta:
+        unique_together = (("subject_one", "subject_two", "subject_three"),("primary_language","secondary_language"),)
+####################
+## Profiles Model
 class Profiles(models.Model):
     school_id = models.ForeignKey(Schools)
     name = models.CharField(max_length=30)
     is_available = models.BooleanField()
-
-class Languages(models.Model):
-    #nie wiem czy po tym bedzie schools?
-    school_id = models.ForeignKey(Schools)
-    name = models.CharField(max_length=30)
-    is_available = models.BooleanField()
-
-class Subjects(models.Model):
-    is_available = models.BooleanField()
-    wage = models.IntegerField()
