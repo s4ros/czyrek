31cf4cbb28eb425a3f93d1d9a89e211d07760728
emi <3godlewska@gmail.com>
2017-03-09 22:17:06 +0100

    added models: Schools, Profiles, Subjects

--- czyrek/czyrek/manager/models.py
+++ czyrek/czyrek/manager/models.py
@@ -12,3 +12,18 @@ class User(models.Model):
 
     def __str__(self):
         return self.login + " (" + self.email + ")" + " - " + self.full_name
+
+class Schools(models.Model):
+    name = models.CharField(max_length=100)
+    is_available = models.BooleanField()
+
+class Profiles(models.Model):
+    school_id = models.ForeignFey(Schools)
+    name = models.CharField(max_length=30)
+    is_available = models.BooleanField()
+
+#class Languages(models.Model):
+
+class Subjects(models.Model):
+    is_available = models.BooleanField()
+    wage = models.IntegerField()
