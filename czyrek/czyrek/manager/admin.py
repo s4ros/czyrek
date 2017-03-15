c093e5e574d70fa104ba4b3d661ffcdc8b387697
Lukasz Wojno <l.wojno@imagho.pl>
2017-03-15 20:29:12 +0100

    Added all czyrek models to admin site.

--- czyrek/czyrek/manager/admin.py
+++ czyrek/czyrek/manager/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from czyrek.manager.models import Candidate
+from czyrek.manager.models import *
 
 # Register your models here.
 admin.site.register(Candidate)
+admin.site.register(Schools)
+admin.site.register(Languages)
+admin.site.register(Subjects)
+admin.site.register(Profiles)
